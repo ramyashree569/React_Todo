53e7a46e5adf908010d3969c830b5a1b2ab7cb46
ramyashree569 <ramyashree56977@gmail.com>
2020-02-17 13:04:10 +0530

    Updates

---
+++ precache-manifest.bd5d133809af93f98fc68dc64a81e027.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4672b44c8f79e5c5b130c659b63cb6c1",
+    "url": "/React_Todo/index.html"
+  },
+  {
+    "revision": "5931650ff5bcf7bf0f2a",
+    "url": "/React_Todo/static/css/main.33dfaa18.chunk.css"
+  },
+  {
+    "revision": "3155d5d9528c49cb7d3c",
+    "url": "/React_Todo/static/js/2.d87bcce2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React_Todo/static/js/2.d87bcce2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5931650ff5bcf7bf0f2a",
+    "url": "/React_Todo/static/js/main.79059b7c.chunk.js"
+  },
+  {
+    "revision": "e5ce372484113b6b6799",
+    "url": "/React_Todo/static/js/runtime-main.59f82d27.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React_Todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
